bb95cca97194ffd752c0fed1fae1dc125bb93702
fahritech04 <fahricoding@gmail.com>
2025-06-02 13:47:16 +0700

    (Keamanan) : add clickjacking

---
+++ web_gassor/app/Http/Middleware/PreventClickjacking.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class PreventClickjacking
+{
+    /**
+     * Handle an incoming request.
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $response = $next($request);
+        $response->headers->set('X-Frame-Options', 'DENY');
+        $response->headers->set('Content-Security-Policy', ", frame-ancestors 'none'");
+        return $response;
+    }
+}
