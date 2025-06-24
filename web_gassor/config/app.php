f2beb4b9586e6944755466475841f4259904f26c
raihanfahrifi_ <73181745+fahritech04@users.noreply.github.com>
2025-06-24 12:11:29 +0000

    tes

--- web_gassor/config/app.php
+++ web_gassor/config/app.php
@@ -55,7 +55,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'https://localhost'),
+    'url' => env('APP_URL', 'https://gassor-gassewamotor-production.up.railway.app'),
 
     'asset_url' => env('ASSET_URL'),
 
